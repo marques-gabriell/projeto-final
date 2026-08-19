2171fe43269c49c1e57e090a4eade9ef696d6d06
Gabriel M <magicomnstr@gmail.com>
2026-08-18 21:03:26 -0300

    Criação do Repository

---
+++ src/main/java/pokedex/repository/PokemonRepository.java
@@ -0,0 +1,9 @@
+package pokedex.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pokedex.entity.PokemonEntity;
+
+@Repository
+public interface PokemonRepository extends JpaRepository<PokemonEntity, Long> {
+}
