cec6e61fcf4af1486e9fd62139d13e653c083c73
Gabriel M <magicomnstr@gmail.com>
2026-08-19 20:33:15 -0300

    Atualização do Entity

--- src/main/java/com/example/pokedex/entity/PokemonEntity.java
+++ src/main/java/com/example/pokedex/entity/PokemonEntity.java
@@ -1,10 +1,13 @@
 package com.example.pokedex.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@Entity
+@Table(name ="pokemon")
 public class PokemonEntity {
 
     @Id
