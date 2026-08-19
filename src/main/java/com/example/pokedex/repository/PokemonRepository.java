c44b0d311d5d35d5ad5dc0adb33d787af9b1a312
Gabriel M <magicomnstr@gmail.com>
2026-08-19 20:17:02 -0300

    Atualização do Repository

--- src/main/java/com/example/pokedex/repository/PokemonRepository.java
+++ src/main/java/com/example/pokedex/repository/PokemonRepository.java
@@ -1,8 +1,8 @@
-package pokedex.repository;
+package com.example.pokedex.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pokedex.entity.PokemonEntity;
+import com.example.pokedex.entity.PokemonEntity;
 
 @Repository
 public interface PokemonRepository extends JpaRepository<PokemonEntity, Long> {
