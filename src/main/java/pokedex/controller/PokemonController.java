4650c433f532dd06a9a62819ce54822736e01319
Gabriel M <magicomnstr@gmail.com>
2026-08-18 21:23:55 -0300

    Criação do Controller

---
+++ src/main/java/pokedex/controller/PokemonController.java
@@ -0,0 +1,38 @@
+package pokedex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pokedex.entity.PokemonEntity;
+import pokedex.repository.PokemonRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pokemons")
+@CrossOrigin(origins = "*")
+
+public class PokemonController {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    @GetMapping
+    public List<PokemonEntity> listarPokemons() {
+        return pokemonRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PokemonEntity> buscarPokemon(@PathVariable Long id) {
+        Optional<PokemonEntity> pokemon = pokemonRepository.findById(id);
+
+        if (pokemon.isPresent()) {
+            return ResponseEntity.ok(pokemon.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<PokemonEntity> criarPokemon(
+}
