10e6d9f6d42fef9019b4ebbc3a116b37cd76afa2
Gabriel M <magicomnstr@gmail.com>
2026-08-18 21:03:12 -0300

    Atualização do Entity

---
+++ src/main/java/pokedex/entity/PokemonEntity.java
@@ -0,0 +1,78 @@
+package pokedex.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+public class PokemonEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String nome;
+    private String tipo;
+    private Double altura;
+    private Double peso;
+    private String imagem;
+
+    public PokemonEntity() {
+
+    }
+
+    public PokemonEntity(String nome, String tipo, Double altura, Double peso, String imagem){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.peso = peso;
+        this.imagem = imagem;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}
